66fcbc2f4f0ac154704fa3554621643cd838765b
aleksejpiskunov <piskunovalxy@gmail.com>
2020-10-04 17:49:26 +0300

    Flask + SQLAlchemy #2

---
+++ auth.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from flask_login import login_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html',error='None')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    login_user(user)
+    # if the above check passes, then we know the user has the right credentials
+    return redirect(url_for('main.test_suites'))
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    token = request.form.get('token')
+
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'), token=token)
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
