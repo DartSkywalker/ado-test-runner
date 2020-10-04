5a26aedfe380532499fe8e4ad2706b044f51b9e7
aleksejpiskunov <piskunovalxy@gmail.com>
2020-10-04 14:53:00 +0300

    gitignore

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,6 @@
-ADO_TOKEN = "felvwh7xjalzfxici5ivexzktjnjcablwsvc2ab2zmulqkke5yta"
+from crelds import ADO_TOKEN
+
+ADO_TOKEN = ADO_TOKEN
 QUERY_ID = "abb94139-79de-4924-b2f1-73468d05fc20"
 QUERY_LINK = "https://dev.azure.com/HAL-LMKRD/RESDEV/_apis/wit/queries/"
 WIQL_LINK = "https://dev.azure.com/HAL-LMKRD/RESDEV/_apis/wit/wiql/"
