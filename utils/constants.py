f54ea24eeb8545731f33be2c9fd8e88a418a5f7a
huuungry <Huuungry@yandex.ru>
2020-10-05 10:31:00 +0300

    Get ADO_TOKEN from the User environment variable instead of the file

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,12 @@
-from ..crelds import ADO_TOKEN
+# from ..crelds import ADO_TOKEN
+import os
 
-ADO_TOKEN = ADO_TOKEN
+try:
+   ADO_TOKEN = os.environ["ADO_TOKEN"]
+   # print(ADO_TOKEN)
+except KeyError:
+   print("Please set the environment variable ADO_TOKEN")
+
 QUERY_ID = "abb94139-79de-4924-b2f1-73468d05fc20"
 QUERY_LINK = "https://dev.azure.com/HAL-LMKRD/RESDEV/_apis/wit/queries/"
 WIQL_LINK = "https://dev.azure.com/HAL-LMKRD/RESDEV/_apis/wit/wiql/"
