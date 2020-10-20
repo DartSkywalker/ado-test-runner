9ab0297267683aa8a5c6b63c74fdfc6d3115e2cf
aleksejpiskunov <piskunovalxy@gmail.com>
2020-10-20 08:40:52 +0300

    Flask routes changes to handle suite_id and test_case_id in url

--- main.py
+++ main.py
@@ -7,41 +7,57 @@ from .models_data import create_table
 main = Blueprint('main', __name__)
 create_table()
 
+
 @main.route('/suites', methods=['GET'])
 @login_required
 def test_suites():
-    return render_template('index.html', test_suite_list=ado_api.get_test_suites_from_database(), username=ado_api.get_current_user())
+    return render_template('index.html', test_suite_dict=ado_api.get_test_suites_from_database(),
+                           username=ado_api.get_current_user())
+
 
 @main.route('/suites', methods=['POST'])
 @login_required
 def add_test_suite():
     if request.form["btn"] == "selectSuite":
         if request.method == 'POST':
-            return redirect(url_for('main.test_cases_list', test_suite_name=request.form.get('test_suites')))
+            return redirect('/cases/' + request.form.get('test_suites'))
+            # return redirect(url_for('main.test_cases_list', test_suite_name=request.form.get('test_suites')))
     query_id = request.form['query_id']
     ado_api.create_new_test_suite_in_db(str(query_id))
     return redirect(url_for('main.test_suites'))
 
-@main.route('/cases', methods=['GET', 'POST'])
+
+@main.route('/cases/<suite_id>', methods=['GET', 'POST'])
 @login_required
-def test_cases_list():
-    test_suite_name = request.args.get('test_suite_name')
-    test_cases_dict = ado_api.get_test_cases_from_db_by_suite_name(test_suite_name)
-    return render_template('test_cases_list.html', test_suite_name=test_suite_name, test_cases_dict=test_cases_dict, username=ado_api.get_current_user(),
+def test_cases_list(suite_id):
+    test_cases_dict = ado_api.get_test_cases_from_db_by_suite_name(suite_id)
+    test_suite_name = ado_api.get_test_suite_name_by_id(suite_id)
+    return render_template('test_cases_list.html', test_suite_name=test_suite_name, test_cases_dict=test_cases_dict,
+                           username=ado_api.get_current_user(),
                            users_dict=ado_api.get_all_users())
+
 
 @main.route('/about', methods=['GET', 'POST'])
 def about_page():
     if current_user.is_authenticated:
-         return render_template("about.html", username=ado_api.get_current_user())
+        return render_template("about.html", username=ado_api.get_current_user())
     else:
-         return render_template("about_not_auth.html")
+        return render_template("about_not_auth.html")
+
 
 @main.route('/')
 def redirect_from_main():
     return redirect(url_for('main.test_suites'))
 
-@main.route('/run')
-# @main.route('/run/<test_id>')
-def test_run():
+
+@main.route('/save_test_result', methods=['POST'])
+def save_test_result():
+    data = request.get_json()
+    print(data)
+    return ("200")
+
+
+# @main.route('/run')
+@main.route('/run/<test_suite_id>/<test_case_id>')
+def test_run(test_suite_id, test_case_id):
     return render_template("run.html", test_case_name='TEST_CASE_NAME')
--- utils/api/ado_api.py
+++ utils/api/ado_api.py
@@ -13,12 +13,14 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.sql import table, column, select, update, insert
 from sqlalchemy import Table, MetaData, create_engine
 
+
 def sql_connection():
     engine = create_engine('sqlite:///db.sqlite', echo=True)
     connection = engine.connect()
     meta = MetaData()
     meta.reflect(bind=engine)
     return connection, meta
+
 
 def create_db_connection(db_file):
     """
@@ -31,6 +33,7 @@ def create_db_connection(db_file):
     except Error as e:
         logger.critical(f"Cannot connect to {db_file} database")
 
+
 def get_query_name_by_query_id(query_id):
     """
     Returns query name by its id
@@ -42,6 +45,7 @@ def get_query_name_by_query_id(query_id):
         r_query.close()
         parsed_data = json.loads(str(r_query.text))
         return parsed_data['name']
+
 
 def get_test_cases_urls_by_query_id(query_id):
     """
@@ -59,6 +63,7 @@ def get_test_cases_urls_by_query_id(query_id):
     else:
         logger.critical(f"ADO returns status code {str(r_query.status_code)}. Check your ADO_TOKEN.")
 
+
 def get_test_case_name(tc_id):
     r_query = requests.get(WORKITEM_LINK + str(tc_id), headers=HEADERS, auth=('', ADO_TOKEN))
     if r_query.status_code == 200:
@@ -68,6 +73,7 @@ def get_test_case_name(tc_id):
         return str(test_case_name)
     else:
         logger.critical(f"ADO returns status code {str(r_query.status_code)}. Check your ADO_TOKEN.")
+
 
 def get_test_case_steps_by_url(test_case_url):
     """
@@ -86,6 +92,8 @@ def get_test_case_steps_by_url(test_case_url):
         # print(steps)
         steps_list = ado_parser.parse_html_steps(steps)
         return steps_list
+
+
 # print(get_test_case_steps_by_url("https://dev.azure.com/HAL-LMKRD/d54c5f94-240d-4817-b74e-82588f96c6ba/_apis/wit/workItems/128710"))
 
 def get_all_test_case_data(tc_id):
@@ -105,8 +113,10 @@ def get_all_test_case_data(tc_id):
             steps = "Test Case does not contain steps"
         # print(steps)
         steps_list = ado_parser.parse_html_steps(steps)
-        test_case = [test_case_title,steps_list]
+        test_case = [test_case_title, steps_list]
         return test_case
+
+
 # print(get_all_test_case_data(128597))
 
 
@@ -128,13 +138,14 @@ def create_new_test_suite_in_db(query_id):
                                                                   CREATED_BY=str(user[0][0])))
 
     test_suite_id = connection.execute(select([table_suites.columns['TEST_SUITE_ID']])
-                                       .where(table_suites.columns['TEST_SUITE_NAME'] == str(test_suite_name))).fetchone()[0]
+                                       .where(
+        table_suites.columns['TEST_SUITE_NAME'] == str(test_suite_name))).fetchone()[0]
 
     for id, url in test_cases_dict.items():
         logger.debug(str(id), url, test_suite_name)
         test_case = get_all_test_case_data(str(id))
         test_case_name = test_case[0]
-        step_number=1
+        step_number = 1
 
         connection.execute(meta.tables['TEST_CASES'].insert().values(TEST_SUITE_ID=str(test_suite_id),
                                                                      TEST_CASE_ADO_ID=str(id),
@@ -142,40 +153,66 @@ def create_new_test_suite_in_db(query_id):
                                                                      STATUS='Ready'))
         for test_steps in test_case[1]:
             test_sql_case_id = connection.execute(select([table_cases.columns['TEST_CASE_ID']])
-                .where(table_cases.columns['TEST_CASE_ADO_ID'] == id)).fetchone()[0]
+                                                  .where(table_cases.columns['TEST_CASE_ADO_ID'] == id)).fetchone()[0]
             print(test_sql_case_id)
             connection.execute(meta.tables['TEST_STEPS'].insert().values(TEST_CASE_ID=int(test_sql_case_id),
                                                                          STEP_NUMBER=str(step_number),
                                                                          DESCRIPTION=test_steps[0],
                                                                          EXPECTED_RESULT=test_steps[1]))
-            step_number+=1
-    logger.info(f"{test_suite_name} was successfully added to the database. Contains {len(test_cases_dict)} test cases.")
+            step_number += 1
+    logger.info(
+        f"{test_suite_name} was successfully added to the database. Contains {len(test_cases_dict)} test cases.")
+
+
 # create_new_test_suite_in_db("1f70f015-030a-48ca-9674-4bfd123c801c")
 
+# def get_test_suites_from_database():
+#     connection, meta = sql_connection()
+#     suites = Table('TEST_SUITES', meta)
+#     test_suites_list_db = connection.execute(select([suites.columns['TEST_SUITE_NAME']]).distinct()).fetchall()
+#     test_suites_list = [suite[0] for suite in test_suites_list_db]
+#     return test_suites_list
 def get_test_suites_from_database():
+    """
+    Return suite with its id's
+    """
     connection, meta = sql_connection()
     suites = Table('TEST_SUITES', meta)
-    test_suites_list_db = connection.execute(select([suites.columns['TEST_SUITE_NAME']]).distinct()).fetchall()
-    test_suites_list = [suite[0] for suite in test_suites_list_db]
-    return test_suites_list
+    test_suites_list_db = connection.execute(select([suites.columns['TEST_SUITE_ID'],
+                                                     suites.columns['TEST_SUITE_NAME']]).distinct()).fetchall()
+    test_suites_ids = [suite[0] for suite in test_suites_list_db]
+    test_suite_names = [suite[1] for suite in test_suites_list_db]
+    return dict(zip(test_suites_ids, test_suite_names))
 
-def get_test_cases_from_db_by_suite_name(test_suite):
+
+def get_test_suite_name_by_id(suite_id):
+    connection, meta = sql_connection()
+    table_suites = Table('TEST_SUITES', meta)
+    suite_name = connection.execute(select([table_suites.columns['TEST_SUITE_NAME']]).
+                                    where(table_suites.columns['TEST_SUITE_ID'] == suite_id)).fetchone()[0]
+    return suite_name
+
+def get_test_cases_from_db_by_suite_name(test_suite_id):
     connection, meta = sql_connection()
     test_cases_table = Table('TEST_CASES', meta)
     table_suites = Table('TEST_SUITES', meta)
-    test_suite_id = connection.execute(select([table_suites.columns['TEST_SUITE_ID']]).
-                                       where(table_suites.columns['TEST_SUITE_NAME'] == test_suite)).fetchone()[0]
+    # test_suite_id = connection.execute(select([table_suites.columns['TEST_SUITE_ID']]).
+    #                                    where(table_suites.columns['TEST_SUITE_NAME'] == test_suite)).fetchone()[0]
 
     test_cases_list_db = connection.execute(select([test_cases_table.columns['TEST_CASE_ADO_ID'],
-                                                 test_cases_table.columns['TEST_CASE_NAME']])
-                                         .where(test_cases_table.columns['TEST_SUITE_ID'] == test_suite_id)).fetchall()
+                                                    test_cases_table.columns['TEST_CASE_NAME']])
+                                            .where(
+        test_cases_table.columns['TEST_SUITE_ID'] == test_suite_id)).fetchall()
 
     test_cases_id_list = [test_case[0] for test_case in test_cases_list_db]
     test_cases_name_list = [test_case[1] for test_case in test_cases_list_db]
-    test_cases_link_list = ["https://dev.azure.com/HAL-LMKRD/RESDEV/_workitems/edit/" + str(tc_id) for tc_id in test_cases_id_list]
+    test_cases_link_list = ["https://dev.azure.com/HAL-LMKRD/RESDEV/_workitems/edit/" + str(tc_id) for tc_id in
+                            test_cases_id_list]
 
     test_case_dict = dict(zip(test_cases_id_list, zip(test_cases_name_list, test_cases_link_list)))
     return test_case_dict
+
+
 # get_test_cases_from_db_by_suite_name('Velocity Test Cases')
 
 def get_current_user():
@@ -186,6 +223,7 @@ def get_current_user():
     user = connection.execute(query).fetchall()
     return str(user[0][0])
 
+
 def get_all_users():
     connection, meta = sql_connection()
     table = Table('User', meta)
@@ -195,3 +233,8 @@ def get_all_users():
     user_names_list = [ids[0] for ids in users]
     users_dict = dict(zip(user_ids_list, user_names_list))
     return users_dict
+
+
+def get_test_case_steps_by_id():
+    # Get Test Suite id
+    pass
