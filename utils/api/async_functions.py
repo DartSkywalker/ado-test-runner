0347ade7fec792d25ea46f34a1ce8c75a45d6f3b
huuungry <Huuungry@yandex.ru>
2020-11-17 08:24:16 +0200

    Fixed some sql connection issues

--- utils/api/async_functions.py
+++ utils/api/async_functions.py
@@ -8,8 +8,8 @@ from loguru import logger
 from sqlalchemy import select
 
 from . import ado_parser
-from .ado_api import get_test_cases_urls_by_query_id, get_query_name_by_query_id, connection
-from .sql_api import table_user, table_suites, table_cases, table_steps, get_current_user
+from .ado_api import get_test_cases_urls_by_query_id, get_query_name_by_query_id
+from .sql_api import table_user, table_suites, table_cases, table_steps, get_current_user, connection
 from ..constants import HEADERS, get_ado_token_for_user
 
 
